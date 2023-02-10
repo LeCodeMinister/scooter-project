becd2a30d1e1e22fbfa6e117fc6a9898edd112af
LeCodeMinister <ahmedalhindi@protonmail.com>
2023-02-10 01:46:14 +0000

    User & Scooter Tests Done

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,74 @@
 const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 
-//typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
-
-//Method tests
-describe('scooter methods', () => {
-  // tests here!
-
-  //rent method
-
-  //dock method
-
-  //requestRepair method
-
-  //charge method
-
-})
+describe("Testing Scooter Class", () => {
+  //Testing station
+  test("Testing 'station' property (parameter passed)", () => {
+      const scooter1 = new Scooter('London Station');
+      expect(scooter1.station).toBe('London Station');
+  });
+  test("Testing 'station' property (parameter NOT passed)", () => {
+      const scooter1 = new Scooter();
+      expect(scooter1.station).toBe(null);
+  });
+  //Testing user
+  test("Testing 'user' property (parameter passed)", () => {
+      const scooter1 = new Scooter('London Station', 'Ahmed');
+      expect(scooter1.user).toBe('Ahmed');
+  });
+  test("Testing 'user' property (parameter NOT passed)", () => {
+      const scooter1 = new Scooter('London Station');
+      expect(scooter1.user).toBe(null);
+  });
+  //Testing nextSerial
+  test("Testing 'nextSerial' property", () => {
+      const scooter1 = new Scooter('London Station', 'Ahmed');
+      expect(scooter1.nextSerial).toBe(2);
+  });
+  test("Testing 'nextSerial' property", () => {
+      const scooter1 = new Scooter('London Station', 'Ahmed');
+      const scooter2 = new Scooter('Manchester Station', 'Steve');
+      expect(scooter2.nextSerial).toBe(3);
+  });
+  //Testing serial
+  test("Testing 'serial' property", () => {
+      const scooter1 = new Scooter('London Station', 'Ahmed');
+      expect(scooter1.serial).toBe(1);
+  });
+  //Testing charge
+  test("Testing 'charge' property", () => {
+      const scooter1 = new Scooter('London Station', 'Ahmed');
+      expect(scooter1.charge).toBe(100);
+  });
+  //Testing isBroken
+  test("Testing 'isBroken' property", () => {
+      const scooter1 = new Scooter('London Station', 'Ahmed');
+      expect(scooter1.isBroken).toBe(false);
+  });
+  //Testing rent()
+  test("Testing 'rent()' method", () => {
+      const scooter1 = new Scooter('London Station', 'Ahmed');
+      expect(scooter1.rent()).toBe(false);
+  });//needs rentScooter() from ScooterApp here
+  //Testing dock(station)
+  test("Testing 'dock(station)' method", () => {
+      const scooter1 = new Scooter('London Station', 'Ahmed');
+      //needs rentScooter() from ScooterApp here
+      scooter1.dock('Manchester Station');
+      expect(scooter1.station).toBe('Manchester Station');
+  });//needs dockScooter() from ScooterApp here
+  //Testing recharge()
+  test("Testing 'recharge()' method", () => {
+      const scooter1 = new Scooter('London Station', 'Ahmed');
+      scooter1.charge = 0;
+      scooter1.recharge();
+      expect(scooter1.charge).toBe(100);
+  });
+  //Testing requestRepair()
+  test("Testing 'requestRepair()' method", () => {
+      const scooter1 = new Scooter('London Station', 'Ahmed');
+      scooter1.isBroken = true;
+      scooter1.requestRepair();
+      expect(scooter1.isBroken).toBe(false);
+  });
+});
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,55 @@
 const User = require('../src/User')
 
 // User tests here
+describe("Testing User Class", () => {
+    //Testing username
+    test("Testing 'username' property", () => {
+        const user1 = new User("Ahmed", "QWERTY123", 18);
+        expect(user1.username).toBe("Ahmed");
+    });
+    //Testing password
+    test("Testing 'password' property", () => {
+        const user1 = new User("Ahmed", "QWERTY123", 18);
+        expect(user1.password).toBe("QWERTY123");
+    });
+    //Testing age
+    test("Testing 'age' property", () => {
+        const user1 = new User("Ahmed", "QWERTY123", 18);
+        expect(user1.age).toBe(18);
+    });
+    //Testing loggedIn
+    test("Testing 'loggedIn' property", () => {
+        const user1 = new User("Ahmed", "QWERTY123", 18);
+        expect(user1.loggedIn).toBe(false);
+    });
+    //Testing login
+    test("Testing 'login' method (correct password)", () => {
+        const user1 = new User("Ahmed", "QWERTY123", 18);
+        user1.login("QWERTY123");
+        expect(user1.loggedIn).toBe(true);
+    });
+    test("Testing 'login' method (wrong password)", () => {
+        const user1 = new User("Ahmed", "QWERTY123", 18);
+        user1.login("QWERTY");
+        expect(user1.loggedIn).toBe(false);
+    });
+    //Testing logout
+    test("Testing 'logout' method (user exists)", () => {
+        const user1 = new User("Ahmed", "QWERTY123", 18);
+        user1.login("QWERTY123");
+        user1.logout();
+        expect(user1.loggedIn).toBe(false);
+    });
+    test("Testing 'logout' method (user does not exist)", () => {
+        const user1 = new User("Ahmed", "QWERTY123", 18);
+        user1.login("QWERTY123");
+        //*****
+        //Tampering with the user's key:value pair on purpose
+        delete registeredUsers[`${user1.username}`];
+        //*****
+        user1.logout();
+        expect(user1.loggedIn).toBe(false);
+        //*Don't know how to test for the logout value, fix this at some point*//
+    });
 
-// test username
-
-// test password
-
-// test age
-
-// test login
-
-// test logout
+});
